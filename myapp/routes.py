67869b52aa7eeeb6a88ff4e2700be27811055a8e
ismcodifiquer <60313089+ismcodifiquer@users.noreply.github.com>
2020-02-11 19:31:56 +0100

    Posible carga de datos dinamicos desde csv en index

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,9 +1,39 @@
 from myapp import app
 from flask import render_template
+import csv
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    '''
+    leer el fichero sales10.csv y transformarlo en un diccionario
+    '''
+    fSales = open('../data/sales10.csv', 'r')
+
+    csvreader = csv.reader(fSales, delimiter=',')
+    registros = []
+    for linea in csvreader:
+        registros.append(linea)
+
+    cabecera = registros[0]
+    
+    ventas = []
+    for datos in registros[1:]:
+        d = {}
+        for ix, nombre_campo in enumerate(cabecera):
+            d[nombre_campo] = datos[ix]
+        ventas.append(d)   
+
+    '''
+    procesarlo para obtener los totales
+    '''
+    resultado = [] #lista de diccionarios
+
+    '''
+    enviarlo a index.html
+    '''
+
+
+    return render_template('index.html', registros=resultado)
 
 @app.route('/detail')
 def detail():
