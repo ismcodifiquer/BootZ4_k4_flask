c02a9e712cd47ab1b80ac1a8112f4d22a661e3c5
ismcodifiquer <60313089+ismcodifiquer@users.noreply.github.com>
2020-02-11 09:17:20 +0100

    hola mundo con flask

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hola, mundo'
+
+@app.route('/otrorecurso')
+def otro():
+    return '''
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Mi pagina</title>
+        </head>
+        <body>
+            <h1>Mi saludo</h1>
+            <p>Hola, mundo</p>
+        </body>
+    </html>
+    '''
