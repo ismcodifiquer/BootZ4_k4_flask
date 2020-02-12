51b90927d87f7995b79eff04d7a8fcfc8ed717fc
ismcodifiquer <60313089+ismcodifiquer@users.noreply.github.com>
2020-02-12 16:35:59 +0100

    Detalles con parametros en ruta y css

--- myapp/routes.py
+++ myapp/routes.py
@@ -2,31 +2,47 @@ from myapp import app
 from flask import render_template
 import csv
 
-@app.route('/')
-def index():
-    '''
-    leer el fichero sales10.csv y transformarlo en un diccionario
-    '''
-    fSales = open('../data/sales10.csv', 'r')
 
-    csvreader = csv.reader(fSales, delimiter=',')
-    registros = []
-    for linea in csvreader:
-        registros.append(linea)
+'''
+leer el fichero sales10.csv y transformarlo en un diccionario
+'''
 
-    cabecera = registros[0]
+fSales = open('./data/sales10.csv', 'r')
+
+csvreader = csv.reader(fSales, delimiter=',')
+registros = []
+for linea in csvreader:
+    registros.append(linea)
+
+cabecera = registros[0]
     
-    ventas = []
-    for datos in registros[1:]:
-        d = {}
-        for ix, nombre_campo in enumerate(cabecera):
-            d[nombre_campo] = datos[ix]
-        ventas.append(d)   
+ventas = []
+for datos in registros[1:]:
+    d = {}
+    for ix, nombre_campo in enumerate(cabecera):
+        d[nombre_campo] = datos[ix]
+    ventas.append(d) 
 
+@app.route('/')
+def index():    
     '''
     procesarlo para obtener los totales
     '''
+    datos = {}
+    for linea in ventas:
+        if linea['region'] in datos:
+            regAct = datos[linea['region']]
+            regAct['ingresos_totales'] += float(linea['ingresos_totales'])
+            regAct['beneficios_totales'] += float(linea['beneficio'])
+        else:
+            datos[linea['region']] = {'ingresos_totales': float(linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
+
+
     resultado = [] #lista de diccionarios
+    for clave in datos:
+        resultado.append((clave, datos[clave]))
+    
+    
 
     '''
     enviarlo a index.html
@@ -35,7 +51,18 @@ def index():
 
     return render_template('index.html', registros=resultado)
 
-@app.route('/detail')
-def detail():
-    return render_template('detail.html')
+@app.route('/detail/<region_name>')
+def detail(region_name):
+
+    datos = {}
+    for linea in ventas:
+        if linea['region'] == region_name:
+            if linea['pais'] in datos:
+                regAct = datos[linea['pais']]
+                regAct['ingresos_totales'] += float(linea['ingresos_totales'])
+                regAct['beneficios_totales'] += float(linea['beneficio'])
+            else:
+                datos[linea['pais']] = {'ingresos_totales': float(linea['ingresos_totales']), 'beneficios_totales': float(linea['beneficio'])}
+
+    return render_template('detail.html', region=region_name, registros=datos)
     
